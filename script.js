c0beba71feed73722071988050c674bf143ebb55
anand <24557+and@users.noreply.github.com>
2023-05-15 20:10:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('Hello from Nerdbord!')
+
+setInterval(changeDivBorderColor, 1000);
+
+function changeDivBorderColor(){
+    let box = document.getElementsByClassName('box');
+    let colors = ['#F97340','#197340','#EA7340', '#AAA340'];
+    console.log(box[0]);
+    let i = Math.floor(Math.random() * 3);
+    box[0].setAttribute('style','border:40px solid '+colors[i]);
+}
