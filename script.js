0f91e095373ddc4381cfa50f886266e99c3b1f5b
anand <24557+and@users.noreply.github.com>
2023-05-15 20:13:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ setInterval(changeDivBorderColor, 1000);
 
 function changeDivBorderColor(){
     let box = document.getElementsByClassName('box');
-    let colors = ['#F97340','#197340','#EA7340', '#AAA340'];
+    let colors = ['#F97340','#197340','#EA7340', '#AAA340', red];
     console.log(box[0]);
-    let i = Math.floor(Math.random() * 3);
+    let i = Math.floor(Math.random() * 4);
     box[0].setAttribute('style','border:40px solid '+colors[i]);
 }
